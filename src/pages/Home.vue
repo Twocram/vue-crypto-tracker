e8d04fd9940b936476cd24a477bf16ce5129ea66
Twocram <2002maslovartem@gmail.com>
2022-05-03 19:24:53 +0500

    added CoinList with props

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Main page</h1>
+  <div>
+    <CoinList :coins="coins"></CoinList>
+  </div>
 </template>
 
 <script>
-export default {};
+import useCoins from '@/hooks/useCoins';
+import CoinList from '@/components/CoinList.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  setup() {
+    const { coins } = useCoins();
+    return {
+      coins,
+    };
+  },
+  components: { CoinList },
+};
 </script>
 
 <style lang="sass" scoped></style>
