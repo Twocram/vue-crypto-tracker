741c709df121c9690601c0ec3faf38c3865ba8cb
Twocram <2002maslovartem@gmail.com>
2022-05-06 20:52:02 +0500

    corrected a typo in the word

--- src/hooks/useCoins.js
+++ src/hooks/useCoins.js
@@ -1,15 +1,15 @@
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from "vue";
 
 export default function useCoins() {
   const coins = ref([]);
   const fetching = async () => {
     try {
       const data = await fetch(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
       ).then((data) => data.json());
       coins.value = data;
     } catch (error) {
-      console.log('Error from fetchin data');
+      console.log("Error from fetching data");
     }
   };
 
