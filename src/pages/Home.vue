5127cf9a3e6317ff5bc8e5e48b216322378d4e2c
Twocram <2002maslovartem@gmail.com>
2022-05-07 17:33:00 +0500

    added properties from useSearchedCoins function

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,35 @@
 <template>
-  <div>
-    <CoinList :coins="coins"></CoinList>
+  <div class="main__wrapper">
+    <custom-input type="text" v-model="searchQuery" />
+    <CoinList :coins="searchedCoins"></CoinList>
   </div>
 </template>
 
 <script>
-import useCoins from "@/hooks/useCoins.js";
-import CoinList from "@/components/CoinList.vue";
+import useCoins from '@/hooks/useCoins.js';
+import CoinList from '@/components/CoinList.vue';
+import useSearchedCoins from '@/hooks/useSearchedCoins.js';
+import CustomInput from '@/components/UI/CustomInput.vue';
 
 export default {
   setup() {
     const { coins } = useCoins();
+    const { searchQuery, searchedCoins } = useSearchedCoins(coins);
     return {
       coins,
+      searchQuery,
+      searchedCoins,
     };
   },
-  components: { CoinList },
+  components: { CoinList, CustomInput },
 };
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.main__wrapper {
+  input {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
