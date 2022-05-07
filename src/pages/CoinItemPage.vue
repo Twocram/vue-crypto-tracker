7cef37cf231259e9902f1ab22cd3f68eb1c5b13d
Twocram <2002maslovartem@gmail.com>
2022-05-07 17:28:43 +0500

    added output info about coin

--- src/pages/CoinItemPage.vue
+++ src/pages/CoinItemPage.vue
@@ -1,8 +1,28 @@
 <template>
-  <button @click="$router.go(-1)">Back</button>
-  <h1>Coin Item page</h1>
+  <div>
+    <button @click="$router.go(-1)">Back</button>
+  </div>
+  <div class="coin-item__wrapper" v-if="!(Object.keys(coinItem).length === 0)">
+    <img class="coin-img" :src="coinItem.img" :alt="coinItem.symbol" />
+    <div>{{ coinItem.currentPrice }}</div>
+    <div>{{ coinItem.name }}({{ coinItem.symbol }})</div>
+    <div>Current Price: {{ coinItem.currentPrice }}$</div>
+    <div>{{ coinItem.totalPrice }}$</div>
+    <div>{{ coinItem.priceChange }}$</div>
+  </div>
+  <div v-else>Loading...</div>
 </template>
 <script>
-export default {};
+import useCoinItem from '@/hooks/useCoinItem';
+import { useRoute } from 'vue-router';
+export default {
+  setup() {
+    const id = useRoute().params.id;
+    const { coinItem } = useCoinItem(id);
+    return {
+      coinItem,
+    };
+  },
+};
 </script>
 <style lang="scss"></style>
