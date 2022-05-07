0abc153b28c243344017a466e5fec4c3b84e9d37
Twocram <2002maslovartem@gmail.com>
2022-05-07 13:14:44 +0500

    added CustomInput component

---
+++ src/components/UI/CustomInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    type="text"
+    :value="modelValue"
+    @input="updateInput"
+    class="custom-input"
+  />
+</template>
+<script>
+export default {
+  name: 'custom-input',
+  props: {
+    modelValue: {
+      type: String,
+    },
+  },
+  methods: {
+    updateInput(e) {
+      this.$emit('update:modelValue', e.target.value);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.custom-input {
+  height: 30px;
+  min-width: 200px;
+  &:focus {
+    outline: none;
+  }
+}
+</style>
